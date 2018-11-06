f09e2ea3fc051994f101ebb6d99b31a1b98ff12b
psroczynski <44477387+psroczynski@users.noreply.github.com>
2018-11-06 13:55:15 +0100

    KArolina Biskup nie potrafi tak prostych rzeczy

--- anagram.cpp
+++ anagram.cpp
@@ -1,30 +1,37 @@
 /*
  * anagram.cpp
+ * 
+ * Ala ma kota, a kot ma Alę!
+ * alA am atok, a tok am ęlA!
  */
-
-
 #include <iostream>
 using namespace std;
-
+// anagram() – funkcja odwraca znaki w pojedynczym wyrazie
+// wyrazy() – funkcja dzieli tekst na wyrazy, znaleziony wyraz
+//            przekazuje do funkcji angram
 int zlicz(char tb[]) {
-        int i = 0;
-        while(tb[i] != '\0') i++;
-        return i;
+    int i = 0;
+    while(tb[i] != '\0') i++;
+    return i;
 }
-
 void wyswietl(char tekst[], int roz) {
-    for(int i = 0; i < roz; i++){
+    for(int i = 0; i < roz; i++) {
         cout << tekst[i];
     }
 }
-
-
+void anagram(char tb[], int roz) {
+    for(int i = roz - 1; i >= 0; i--) {
+        cout << tb[i];
+    }
+}
 int main(int argc, char **argv)
 {
     const int roz = 50;
     char tekst[roz];
-    cin.getline(tekst, 50);
+    cin.getline(tekst, roz);
     wyswietl(tekst, zlicz(tekst));
-	return 0;
+    cout << endl;
+    anagram(tekst, zlicz(tekst));
+    cout << endl;
+    return 0;
 }
-
